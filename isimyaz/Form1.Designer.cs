5b4993743309f74af7380b741954e5fac6adb10b
hilal <130781977+mshelovee@users.noreply.github.com>
2023-09-19 16:19:18 +0300

    Add files via upload

---
+++ isimyaz/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace isimyaz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnYaz = new System.Windows.Forms.Button();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.lblAd = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnYaz
+            // 
+            this.btnYaz.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnYaz.Location = new System.Drawing.Point(115, 142);
+            this.btnYaz.Name = "btnYaz";
+            this.btnYaz.Size = new System.Drawing.Size(139, 67);
+            this.btnYaz.TabIndex = 0;
+            this.btnYaz.Text = "yaz";
+            this.btnYaz.UseVisualStyleBackColor = true;
+            this.btnYaz.Click += new System.EventHandler(this.btnYaz_Click);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTemizle.Location = new System.Drawing.Point(115, 233);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(139, 67);
+            this.btnTemizle.TabIndex = 1;
+            this.btnTemizle.Text = "temizle";
+            this.btnTemizle.UseVisualStyleBackColor = true;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // lblAd
+            // 
+            this.lblAd.AutoSize = true;
+            this.lblAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblAd.Location = new System.Drawing.Point(464, 208);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(42, 25);
+            this.lblAd.TabIndex = 2;
+            this.lblAd.Text = ". . .";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAd);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.btnYaz);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnYaz;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.Label lblAd;
+    }
+}
+
---
+++ isimyaz/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace isimyaz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnYaz_Click(object sender, EventArgs e)
+        {
+            lblAd.Text = "Hilal De Souza";
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            lblAd.Text = " ";
+        }
+    }
+}
